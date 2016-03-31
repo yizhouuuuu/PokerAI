48a58d51a450b87872cf571f9719fe050dddf802
OwenQian <oyqian@uwaterloo.ca>
2016-03-31 00:17:14 -0400

    fixed bug where RNG was only going to 50 and leaving out 51

--- src/poker/Deck.java
+++ src/poker/Deck.java
@@ -15,30 +15,33 @@ public class Deck {
         
     }
     
-    public void makeDeck(){
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 4; j++) {
-                deck[4*i + j] = new Card(i, j);
+    public void makeDeck(){	//assigns the deck to contain every one of the 52 cards
+        for (int i = 0; i <= 12; i++) {	//i is the rank
+            for (int j = 0; j <= 3; j++) {	//j is the suit
+                deck[ 4 * i + j ] = new Card(i, j);
             }
         }
     }
     
-    public Card[] Deal(Card a, Card b) {
+    public Card[] Deal(Card a, Card b) {	//where 'a' and 'b' are your hole cards
         int random;
-        Card[] Dealt = new Card[7];
-        int[] Used = new int[52];
-        Used[4*a.getRank() + a.getSuite()] = 1;
-        Used[4*b.getRank() + b.getSuite()] = 1;
+        Card[] Dealt = new Card[7];	//The 7 cards that are going to be dealt out, 2 opponent hole cards, 5 community
+        int[] Used = new int[52];	//The cards that we've already dealt out
+		//setting hole cards as being dealt out
+        Used[ 4 * a.getRank() + a.getSuite() ] = 1;
+        Used[ 4 * b.getRank() + b.getSuite() ] = 1;
+		//Only doing heads-up so far so 5 com, 2 opponent
         for (int i = 0; i < 7; i++) {
-            random = (int) Math.floor(Math.random()*51);
-                if (Used[random] == 1) {
-                    while (Used[(random + 1) % 52] == 1) {
+            random = (int) Math.floor(Math.random() * 52);	//math.random outputs from [0,1)
+                if (Used[random] == 1) {	//if card is already taken, go to the next card until we find one that's not taken
+                    while (Used[(random + 1) % 52] == 1) {	//ends when the next card isn't taken
                         random++;
                     }
+					//putting the card into dealt and marking it as used
                     Dealt[i] = deck[(random + 1) % 52];
                     Used[(random + 1) % 52] = 1;
                 }
-                else {
+                else {	//if it's not used then immediately deal it out
                     Dealt[i] = deck[random % 52];
                     Used[random] = 1;
                 }
@@ -46,3 +49,4 @@ public class Deck {
         return Dealt;
     }
 }
+
